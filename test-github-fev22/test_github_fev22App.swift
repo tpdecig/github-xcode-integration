13f53cdaa60846e4123253f3f366a65d8357f452
Vincent Berry <vberry@lirmm.fr>
2022-02-17 14:33:15 +0100

    Initial Commit

---
+++ test-github-fev22/test_github_fev22App.swift
@@ -0,0 +1,17 @@
+//
+//  test_github_fev22App.swift
+//  test-github-fev22
+//
+//  Created by vincent Berry on 17/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct test_github_fev22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
